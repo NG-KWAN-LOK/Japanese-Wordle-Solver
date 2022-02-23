af1ce3a999dd66ee43addc02027954668def81da
kwanlok.ng <kwanlok.ng@junyiacademy.org>
2022-02-24 00:53:08 +0800

    update title.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import styles from "./style.module.scss";
 import Header from "./components/Header";
 import Home from "./components/Home";
 function App() {
-  document.title = "Kwan Lok NG";
+  document.title = "Kotobade Asobou Solver 言葉で遊ぼうソルバー";
   return (
     <div className={styles.container}>
       <Header />
